2bb51c52085e625fdd8df739f47c898e441d34e8
crosgar <cecilia.rossi.garcia@gmail.com>
2021-07-24 21:12:18 -0700

    added date format logic

--- models/Thought.js
+++ models/Thought.js
@@ -1,52 +1,54 @@
 const { Schema, model, Types } = require('mongoose');
-//utils date format
+const dateFormat = require("../utils/dateFormat");
 
 const ReactionSchema = new Schema({
-   reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId()
-   },
-   reactionBody: {
-      type: String,
-      required: true,
-      maxLength: 280
-   },
-   username: {
-      type: String,
-      required: true
-   },
-   createdAt: {
-      type: Date,
-      default: Date.now,
-      //get format
-   }
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  },
 });
 
-const ThoughtSchema = new Schema({
-   thoughtText: {
-      type: String, 
-      required: 'Some text required to continue',
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Some text required to continue",
       minLength: 1,
-      maxLength: 280
-   },
-   createdAt: {
+      maxLength: 280,
+    },
+    createdAt: {
       type: Date,
       default: Date.now,
-      //format date
-   },
-   username: {
+      get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
       type: String,
-      required: true
-   },
-   reactions: [ReactionSchema]
-},
-{
-   toJSON: {
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
       virtuals: true,
-      getters: true
-   },
-   id: false
-});
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 //virtual reactionCount, length of reactions array
 ThoughtSchema.virtual('reactionCount').get(function() {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-//utils date format
 
 const UserSchema = new Schema({
    username: {
